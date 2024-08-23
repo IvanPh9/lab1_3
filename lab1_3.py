1b17446d4f61406f2b6478c3360ae80176cfa544
IvanPh9 <igruin12@gmail.com>
2024-08-23 21:26:58 +0300

    1

--- lab1_3.py
+++ lab1_3.py
@@ -1,15 +1,14 @@
-n = 0
-while n <= 0 or n >= 9:
+while True:
     try:
         n = int(input("Введіть висоту пірамідки - "))
         if n <= 0 or n >= 9:
             print("Висота не підлягає діапазону (0<n<9)")
+        else:
+            break
     except ValueError:
         print("Невірний тип розміру пірамідки")
 
-
+strings = ''
 for i in range(n-1, -1, -1):
-    print(' ' * (i*2), end='')
-    for j in range(i+1, n+1):
-        print(j, end=' ')
-    print()
+    strings += ' ' * (i*2) + ' '.join(str(j) for j in range(i+1, n+1))  + '\n'
+print(strings)
